11634806b53140c3a212038de8e514ce30645f13
SantiagoChirinos <sachirinos.23@est.ucab.edu.ve>
2025-07-09 21:25:26 -0400

    -fix: corregido a como quería el profesor
    La feature flag va en el servidor, por lo que dividí la función con feature flag en 2 que cumplen los propósitos de la otra

--- src/libreria.ts
+++ src/libreria.ts
@@ -1,10 +1,8 @@
-import {organizar} from './featureFlag';
-
-export function organizarNombres(nombres:string[], include:string, exclude:string|null): string[]{
-    if(organizar.verificarCondicion(nombres)){
-        return nombres.filter(nombre=>nombre.search(include)!=-1 && (nombre.search(exclude)==-1||!exclude));
-    }
-    else{
-        return nombres.filter(nombre=>nombre.search(include)!=-1);
-    }
+export function buscarNombres(nombres:string[], include:string):string[]{
+    return nombres.filter(nombre=>nombre.search(include)!=-1);
 }
+
+export function buscarYFiltrarNombres(nombres:string[], include:string, exclude:string|null):string[]{
+    return nombres.filter(nombre=>nombre.search(include)!=-1 && (nombre.search(exclude)==-1||!exclude));
+}
+
